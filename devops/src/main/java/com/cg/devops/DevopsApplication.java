538aeb40f4dc4394d77e888c1966b6a99adbdec7
MishraKD <47573746+MishraKD@users.noreply.github.com>
2019-03-06 12:59:00 +0530

    Update DevopsApplication.java

--- devops/src/main/java/com/cg/devops/DevopsApplication.java
+++ devops/src/main/java/com/cg/devops/DevopsApplication.java
@@ -1,11 +1,21 @@
 package com.cg.devops;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class DevopsApplication {
 
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		System.out.println("first docker file to create docker images");
 		SpringApplication.run(DevopsApplication.class, args);
